5855aabea1b66422701e643b70856a1233927c89
Miloš Lapiš <mlapis@mlc.cz>
2022-04-22 10:48:30 +0200

    wip(seed): select

--- src/migrations/2617378125500-Seed.ts
+++ src/migrations/2617378125500-Seed.ts
@@ -5,6 +5,7 @@ export class Seed2617378125500 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     console.log('Seed Up:', queryRunner);
+    await queryRunner.query('SELECT * FROM migrations');
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
